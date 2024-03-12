4c715b7f9b5fc1e4a86483228108c7a48cf2f8c6
PARLI4MENT <ilya6joker@gmail.com>
2024-03-12 16:58:40 +1000

    # Less 14 - Complete

--- Less_14/Program.cs
+++ Less_14/Program.cs
@@ -10,9 +10,40 @@ namespace Less_14
 {
     internal class Program
     {
+        static void RemoveAt(ref int[] arr, int index)
+        {
+            if (index <= arr.Length)
+            {
+                int[] endArr = new int[arr.Length - 1];
+                for (int i = 0; i < index; i++)
+                    endArr[i] = arr[i];
+                for (int i = index + 1; i < arr.Length; i++)
+                    endArr[i - 1] = arr[i];
+
+                arr = endArr;
+            }
+            else { Console.WriteLine("Index out of range array."); return; }
+        }
+        static void RemoveFirst(ref int[] arr)
+        {
+            RemoveAt(ref arr, 0);
+        }
+        static void RemoveEnd(ref int[] arr)
+        {
+            RemoveAt(ref arr, arr.Length -1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] baseArr = { 1, 2, 3, 4, 5 };
+            RemoveAt(ref baseArr, 6);
+            for (int i = 0;i < baseArr.Length; i++)
+            {
+                Console.WriteLine($"{i} {baseArr[i]}");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
