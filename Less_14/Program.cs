dd4d5acf2a1e149de91caefef01f9953c3b98aab
PARLI4MENT <ilya6joker@gmail.com>
2024-03-12 16:35:42 +1000

    Add project files.

---
+++ Less_14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Less_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
